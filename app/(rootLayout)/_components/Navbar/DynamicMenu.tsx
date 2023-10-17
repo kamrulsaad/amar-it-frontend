d4204e78a298beacecf223622b48176d05c0462c
Kamrul Saad <skamrul2680@gmail.com>
2023-10-17 22:05:53 +0600

    Working on navbar

--- app/(rootLayout)/_components/Navbar/DynamicMenu.tsx
+++ app/(rootLayout)/_components/Navbar/DynamicMenu.tsx
@@ -1,8 +1,52 @@
 "use client";
 
-import { isLoggedIn } from "@/services/auth.service";
-import { Button } from "antd";
+import { getUserInfo, isLoggedIn } from "@/services/auth.service";
+import { Button, Menu, MenuProps } from "antd";
+import {
+  UserOutlined,
+  LogoutOutlined,
+  PicLeftOutlined,
+} from "@ant-design/icons";
 import Link from "next/link";
+import dynamic from "next/dynamic";
+
+const user = getUserInfo() as any;
+
+const items: MenuProps["items"] = [
+  {
+    label: "User",
+    key: "SubMenu",
+    icon: <UserOutlined />,
+    children: [
+      {
+        label: (
+          <Link href="/dashboard" target="_blank">
+            Dashboard
+          </Link>
+        ),
+        icon: <PicLeftOutlined />,
+        key: "setting:2",
+      },
+      {
+        label: <Link href="/logout">Log out</Link>,
+        icon: <LogoutOutlined />,
+        key: "setting:1",
+      },
+      {
+        label: <Link href={`/${user.role}/profile`}>Dashboard</Link>,
+        key: "setting:3",
+      },
+    ],
+  },
+  // {
+  //   label: (
+  //     <a href="https://ant.design" target="_blank" rel="noopener noreferrer">
+  //       Navigation Four - Link
+  //     </a>
+  //   ),
+  //   key: 'alipay',
+  // },
+];
 
 const DynamicMenu = () => {
   const userLoggedIn = isLoggedIn();
@@ -10,17 +54,19 @@ const DynamicMenu = () => {
   return (
     <div>
       {userLoggedIn ? (
-        <>
-        <Button type="primary" size="large">
-            <Link href="/dashboard">Dashboard</Link>
-        </Button>
-        </>
+        <Menu mode="horizontal" items={items} />
       ) : (
         <>
+          <Button type="primary" size="large">
+            <Link href="/login">Login</Link>
+          </Button>
+          <Button type="primary" size="large">
+            <Link href="/register">Register</Link>
+          </Button>
         </>
       )}
     </div>
   );
 };
 
-export default DynamicMenu;
+export default dynamic(() => Promise.resolve(DynamicMenu), { ssr: false });
