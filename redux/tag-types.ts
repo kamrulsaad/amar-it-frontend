a9ecacf2b2732b8c966bf1ec67b483dc22d08fb0
Kamrul Saad <skamrul2680@gmail.com>
2023-10-17 16:21:29 +0600

    Inititated Project with ANTD and redux

---
+++ redux/tag-types.ts
@@ -0,0 +1,8 @@
+export enum TagTypes {
+    
+  }
+  
+  export const tagTypesList = [
+    
+  ];
+  
---
+++ types/common.ts
@@ -0,0 +1,21 @@
+export interface IMeta {
+  limit: number;
+  page: number;
+  total: number;
+}
+
+export type ResponseSuccessType = {
+  data: any;
+  meta?: IMeta;
+};
+
+export type IGenericErrorMessage = {
+  path: string | number;
+  message: string;
+};
+
+export type IGenericErrorResponse = {
+  statusCode: number;
+  message: string;
+  errorMessages: IGenericErrorMessage[];
+};
