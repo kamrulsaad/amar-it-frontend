b8ddf25d8b72ef53c66d7b224e8a13d9c40a321b
Noormohammad011 <noormohammad.net1503011@gmail.com>
2023-10-18 04:03:53 +0600

    customer registration ok

--- schemas/user.ts
+++ schemas/user.ts
@@ -6,5 +6,58 @@ export const LoginValidation = z.object({
   password: z.string().min(3).max(255),
 })
 
+
+const CustomerSignUpValidation = z.object(
+  {
+    user: z.object(
+      {
+        username: z.string({
+          required_error: 'Username is required',
+        }),
+        password: z.string({
+          required_error: 'Password is required',
+        }),
+      },
+      {
+        required_error: 'User info is required',
+      }
+    ),
+    customer: z.object(
+      {
+        firstName: z.string({
+          required_error: 'First name is required',
+        }),
+        middleName: z.string().optional(),
+        lastName: z.string({
+          required_error: 'Last name is required',
+        }),
+        email: z
+          .string({
+            required_error: 'Email is required',
+          })
+          .email(),
+        contactNo: z.string({
+          required_error: 'Contact number is required',
+        }),
+        address: z.string({
+          required_error: 'Address is required',
+        }),
+        profileImage: z.string().optional(),
+      },
+      {
+        required_error: 'Customer info is required',
+      }
+    ),
+  },
+  {
+    required_error: 'Informations are required',
+  }
+)
+
+
 export type LoginFormType = z.infer<typeof LoginValidation>
 export const loginResolver = zodResolver(LoginValidation)
+
+
+export type CustomerSignUpFormType = z.infer<typeof CustomerSignUpValidation>
+export const customerSignUpResolver = zodResolver(CustomerSignUpValidation)
