145fa2fc78a27f8549bfb1484e8980abb5653440
Kamrul Saad <skamrul2680@gmail.com>
2023-11-02 22:16:48 +0600

    About section added

--- app/(rootLayout)/page.tsx
+++ app/(rootLayout)/page.tsx
@@ -3,18 +3,20 @@ import Service from "./_components/Services/Service";
 import UpcomingService from "./_components/Upcoming/Upcoming";
 import StatisticSection from "./_components/Statistics/Statistics";
 import { Divider } from "antd";
+import About from "./_components/about/About";
 
 const HomePage = () => {
   return (
     <main>
       <Header />
-      {/* <Divider />
-      <Service />
+      <About />
+      {/* <Divider /> */}
+      {/* <Service />
       <Divider />
       <UpcomingService />
       <Divider />
       <StatisticSection />
-      <Divider /> */}
+      <Divider />  */}
     </main>
   );
 };
